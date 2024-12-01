ce09f0f57a0327909a068bfbebf75b2f9b8f0f85
MatthewCaseres <matthewcaseres@outlook.com>
2024-12-01 17:39:08 -0600

    fix GCP bucket uploads

--- src/server/api/routers/card.ts
+++ src/server/api/routers/card.ts
@@ -1,9 +1,32 @@
+import { getVercelOidcToken } from '@vercel/functions/oidc';
+import { ExternalAccountClient } from 'google-auth-library';
 import { z } from "zod";
 import { adminProcedure, createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 import { Storage } from '@google-cloud/storage';
 import { env } from "~/env";
 
-const storage = new Storage();
+const GCP_PROJECT_NUMBER = process.env.GCP_PROJECT_NUMBER;
+const GCP_SERVICE_ACCOUNT_EMAIL = process.env.GCP_SERVICE_ACCOUNT_EMAIL;
+const GCP_WORKLOAD_IDENTITY_POOL_ID = process.env.GCP_WORKLOAD_IDENTITY_POOL_ID;
+const GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID =
+  process.env.GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID;
+
+// Initialize the External Account Client
+const authClient = ExternalAccountClient.fromJSON({
+  type: 'external_account',
+  audience: `//iam.googleapis.com/projects/${GCP_PROJECT_NUMBER}/locations/global/workloadIdentityPools/${GCP_WORKLOAD_IDENTITY_POOL_ID}/providers/${GCP_WORKLOAD_IDENTITY_POOL_PROVIDER_ID}`,
+  subject_token_type: 'urn:ietf:params:oauth:token-type:jwt',
+  token_url: 'https://sts.googleapis.com/v1/token',
+  service_account_impersonation_url: `https://iamcredentials.googleapis.com/v1/projects/-/serviceAccounts/${GCP_SERVICE_ACCOUNT_EMAIL}:generateAccessToken`,
+  subject_token_supplier: {
+    getSubjectToken: getVercelOidcToken,
+  },
+})!;
+
+const storage = new Storage({
+  authClient
+});
+
 const bucket = storage.bucket(env.GOOGLE_CLOUD_BUCKET_NAME);
 
 async function generateAudio(text: string, voice: string) {
